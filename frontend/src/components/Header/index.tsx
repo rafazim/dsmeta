a8299851c1cb906972c9e166d9ab21c22dc55a9b
Rafael Zim <rafael_zim@hotmail.com>
2022-07-11 23:36:23 -0300

    Other components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+import icon from '../../assets/img/Logo.svg'
+import './styles.css'
+
+function Header() {
+    return (
+        <header>
+            <div className="dsmeta-logo-container">
+                <img src={icon} alt="DSMeta" />
+                <h1>DSMeta</h1>
+                <p>
+                    Desenvolvido por
+                    <a href="https://www.instagram.com/rafael.zim">@rafael.zim</a>
+                </p>
+            </div>
+        </header>
+    )
+}
+
+export default Header
