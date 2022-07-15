d2e9dab340be57f35db1d6e78da5b961478b9fcd
Rafael Zim <rafael_zim@hotmail.com>
2022-07-15 17:26:19 -0300

    link git

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -9,7 +9,7 @@ function Header() {
                 <h1>DSMeta</h1>
                 <p>
                     Desenvolvido por
-                    <a href="https://www.instagram.com/rafael.zim">@rafael.zim</a>
+                    <a href="https://github.com/rafazim">Rafael Zim</a>
                 </p>
             </div>
         </header>
